8dd11cfcb324f1bb2630e1ffb77213396162f857
rohan <rohanverma031@gmail.com>
2025-01-22 16:52:53 +0530

    feat: moved reconnection logic handling to client and added comments

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:doki_websocket_client/src/payload/chat-message/chat_message.dart';
 import 'package:doki_websocket_client/src/payload/delete-message/delete_message.dart';
@@ -11,40 +12,87 @@ import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+/// basic typedef similar to [https://api.flutter.dev/flutter/foundation/foundation-library.html]
 typedef AsyncValueGetter<T> = Future<T> Function();
 typedef ValueSetter<T> = Function(T message);
 typedef VoidCallback = Function();
+typedef ValueGetter<T> = T Function();
+
+/// amount of retries before giving up reconnection
+/// this is based on exponential backoff strategy
+/// eg. time before retrying: 2s, 4s, 8s, 16s... (2^limit+1)
 const int limit = 6;
 
 class Client {
   Client({
     required this.url,
     required this.getToken,
-    this.onChatMessageReceived,
-    this.onTypingStatusReceived,
-    this.onEditMessageReceived,
-    this.onDeleteMessageReceived,
-    this.onConnectionTerminated,
-    this.onRetryAttempt,
-  }) : resource = generateRandomString();
+    required this.onChatMessageReceived,
+    required this.onTypingStatusReceived,
+    required this.onEditMessageReceived,
+    required this.onDeleteMessageReceived,
+    required this.onReconnectSuccess,
+    required this.onConnectionClosure,
+  }) : _resource = generateRandomString();
 
+  /// url is the websocket connection uri
   final Uri url;
+
+  /// getToken gives the token that is used by your websocket server
+  /// token can either be accessToken or idToken
   final AsyncValueGetter<String> getToken;
-  final String resource;
 
-  final ValueSetter<ChatMessage>? onChatMessageReceived;
-  final ValueSetter<TypingStatus>? onTypingStatusReceived;
-  final ValueSetter<EditMessage>? onEditMessageReceived;
-  final ValueSetter<DeleteMessage>? onDeleteMessageReceived;
-  final VoidCallback? onConnectionTerminated;
-  final VoidCallback? onRetryAttempt;
+  /// resource part to identify each user session
+  /// even if they are using same account but different device
+  final String _resource;
 
+  /// onChatMessageReceived is invoked when new [ChatMessage] is received
+  /// [ChatMessage] can either be by same user with different resource or from another user
+  final ValueSetter<ChatMessage> onChatMessageReceived;
+
+  /// onTypingStatusReceived is invoked when [TypingStatus] is received
+  /// [TypingStatus] is only send by different user than the current
+  /// in UI just show the typing status for around 5seconds and than remove
+  /// if user is still typing it will send [TypingStatus] every 4 seconds (something lower than view timeout)
+  final ValueSetter<TypingStatus> onTypingStatusReceived;
+
+  /// onEditMessageReceived is invoked when [EditMessage] is received
+  /// [EditMessage] can either be by same user with different resource or from another user
+  /// in client give priority to updating the message
+  final ValueSetter<EditMessage> onEditMessageReceived;
+
+  /// onDeleteMessageReceived is invoked when [DeleteMessage] is received
+  /// [DeleteMessage] can either be only by same user with different resource when using delete for me
+  /// this will be by different user when the user wants to remove the send message
+  /// in client give priority to deleting message
+  final ValueSetter<DeleteMessage> onDeleteMessageReceived;
+
+  /// onReconnectSuccess is invoked when user is successfully reconnected to the websocket server
+  /// this method should be used to refetch the inbox and syncing of the archives after disconnect
+  final VoidCallback onReconnectSuccess;
+
+  /// onConnectionClosure is invoked with the retry function
+  /// client is suppose to check if application is in foreground and have internet connectivity
+  /// and then call the retry callback
+  final ValueSetter<VoidCallback> onConnectionClosure;
+
+  /// socketChannel holds the underlying [IOWebSocketChannel]
+  /// provided by the web_socket_channel package
   IOWebSocketChannel? _socketChannel;
+
+  /// isActive is used by client to check
+  /// if underlying [WebSocket] connection is alive
   bool get isActive => _socketChannel != null;
 
+  /// isManuallyClosed is to prevent retrying of connection after [disconnect] is called
+  /// [disconnect] should be called when exiting the app to free up any used resources
   bool _isManuallyClosed = false;
+
+  /// tries is to manage number of reconnect request to server after termination of the connection
   int _tries = -1;
 
+  /// connect is used to connect to the websocket server
+  /// if an existing connection exists it does nothing
   Future<void> connect() async {
     if (_socketChannel != null) return;
 
@@ -53,7 +101,7 @@ class Client {
       "Authorization": "Bearer $jwtToken",
     };
     Uri connectionUrl = url.replace(queryParameters: {
-      "resource": resource,
+      "resource": _resource,
     });
 
     final websocketChannel = IOWebSocketChannel.connect(
@@ -68,27 +116,32 @@ class Client {
     );
 
     await websocketChannel.ready;
+    if (_tries != -1) {
+      onReconnectSuccess();
+    }
     _tries = -1;
     _socketChannel = websocketChannel;
 
     _socketChannel!.stream.listen(
       _handleServerPayload,
       onError: (error) {
-        if (_tries == -1) _tries = 0;
-        if (onConnectionTerminated != null) onConnectionTerminated!();
-        _socketChannel = null;
-        _handleLostConnection();
+        _handleConnectionClosure();
       },
-      onDone: () {
-        if (_isManuallyClosed) return;
-        if (_tries == -1) _tries = 0;
-        if (onConnectionTerminated != null) onConnectionTerminated!();
-        _socketChannel = null;
-        _handleLostConnection();
-      },
+      onDone: _handleConnectionClosure,
     );
   }
 
+  /// handleConnectionClosure is called by [IOWebSocketChannel] stream
+  /// when onError or onDone is called
+  void _handleConnectionClosure() {
+    if (_isManuallyClosed) return;
+    if (_tries == -1) _tries = 0;
+    _socketChannel = null;
+
+    onConnectionClosure(_handleLostConnection);
+  }
+
+  /// handleLostConnection allows to retry connection to websocket server
   void _handleLostConnection() {
     if (_tries == -1) return;
 
@@ -96,12 +149,16 @@ class Client {
       Duration(seconds: 1 << _tries),
       () async {
         try {
-          print("retry");
           _tries++;
-          if (onRetryAttempt != null) onRetryAttempt!();
           await connect();
         } on WebSocketChannelException catch (_) {
-          if (_tries < limit) _handleLostConnection();
+          if (_tries < limit) {
+            _handleLostConnection();
+          } else {
+            // allow client to try reconnecting if app is in foreground with active internet connection
+            _tries = 0;
+            onConnectionClosure(_handleLostConnection);
+          }
         } catch (e) {
           return;
         }
@@ -109,6 +166,9 @@ class Client {
     );
   }
 
+  /// handleServerPayload decodes the incoming message and
+  /// calls the respective callback for the payload
+  /// for this is using json, later will upgrade to protobuf
   void _handleServerPayload(dynamic payload) {
     // decode payload to map
     final messageMap = jsonDecode(payload) as Map<String, dynamic>;
@@ -118,27 +178,21 @@ class Client {
     switch (type) {
       case PayloadType.chatMessage:
         final message = ChatMessage.fromJSON(messageMap);
-        if (onChatMessageReceived != null) {
-          onChatMessageReceived!(message);
-        }
+        onChatMessageReceived(message);
       case PayloadType.typingStatus:
         final message = TypingStatus.fromJSON(messageMap);
-        if (onTypingStatusReceived != null) {
-          onTypingStatusReceived!(message);
-        }
+        onTypingStatusReceived(message);
       case PayloadType.editMessage:
         final message = EditMessage.fromJSON(messageMap);
-        if (onEditMessageReceived != null) {
-          onEditMessageReceived!(message);
-        }
+        onEditMessageReceived(message);
       case PayloadType.deleteMessage:
         final message = DeleteMessage.fromJSON(messageMap);
-        if (onDeleteMessageReceived != null) {
-          onDeleteMessageReceived!(message);
-        }
+        onDeleteMessageReceived(message);
     }
   }
 
+  /// disconnect closes the underlying websocket connection to the server
+  /// with [status.normalClosure]
   void disconnect() {
     if (_socketChannel == null) return;
 
@@ -147,27 +201,36 @@ class Client {
     _socketChannel = null;
   }
 
-  void sendMessage(ChatMessage message) {
-    if (_socketChannel == null) return;
+  /// sendMessage method is used to send message to particular user
+  bool sendMessage(ChatMessage message) {
+    if (_socketChannel == null) return false;
 
     _socketChannel!.sink.add(message.toJSON());
+    return true;
   }
 
-  void sendTypingStatus(TypingStatus status) {
-    if (_socketChannel == null) return;
+  /// sendTypingStatus method is used to send typing status to particular user
+  bool sendTypingStatus(TypingStatus status) {
+    if (_socketChannel == null) return false;
 
     _socketChannel!.sink.add(status.toJSON());
+    return true;
   }
 
-  void editMessage(EditMessage message) {
-    if (_socketChannel == null) return;
+  /// editMessage method is used to edit user's own message
+  bool editMessage(EditMessage message) {
+    if (_socketChannel == null) return false;
 
     _socketChannel!.sink.add(message.toJSON());
+    return true;
   }
 
-  void deleteMessage(DeleteMessage message) {
-    if (_socketChannel == null) return;
+  /// deleteMessage is used to delete any message for them
+  /// additionally it also allows to delete message for everyone that is our own message
+  bool deleteMessage(DeleteMessage message) {
+    if (_socketChannel == null) return false;
 
     _socketChannel!.sink.add(message.toJSON());
+    return true;
   }
 }
