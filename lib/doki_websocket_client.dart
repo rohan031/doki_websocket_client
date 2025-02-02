359705295330f629cadc5ce091b74e609720cdd3
rohan <rohanverma031@gmail.com>
2025-02-02 16:28:32 +0530

    update: exported user profile action paylaods

--- lib/doki_websocket_client.dart
+++ lib/doki_websocket_client.dart
@@ -7,6 +7,8 @@ export "src/payload/instant-messaging/edit-message/edit_message.dart";
 export "src/payload/instant-messaging/message-subject/message_subject.dart";
 export "src/payload/instant-messaging/typing-status/typing_status.dart";
 export "src/payload/payload_type.dart";
+export "src/payload/user-profile-actions/user-create-root-node/user_create_root_node.dart";
+export "src/payload/user-profile-actions/user-update-profile/user_update_profile.dart";
 export "src/payload/user-related-actions/user-accept-friend-request/user_accept_friend_request.dart";
 export "src/payload/user-related-actions/user-removes-friend-relation/user_removes_friend_relation.dart";
 export "src/payload/user-related-actions/user-send-friend-request/user_send_friend_request.dart";
